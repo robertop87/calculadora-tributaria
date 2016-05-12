6bf0b13ef4b17199d2e057c196573be9efa27856
Luis Roberto Perez Rios <luis.robertop87@gmail.com>
2016-05-12 17:24:53 -0400

    First feature added: Get values for RC-IVA dependent workers

--- CalculadoraTributaria/app/src/main/java/com/alenasoft/caltrib/dependientes/RCIVAActivity.java
+++ CalculadoraTributaria/app/src/main/java/com/alenasoft/caltrib/dependientes/RCIVAActivity.java
@@ -2,14 +2,102 @@ package com.alenasoft.caltrib.dependientes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.alenasoft.caltrib.R;
 
 public class RCIVAActivity extends AppCompatActivity {
 
+    private EditText totalIncomingEditText;
+    private TextView bruteIncomingTextView;
+    private TextView socialSecurityDiscountTextView;
+    private TextView doubleMinimumSalaryTextView;
+    private TextView diffToDoubleMinimumSalaryTextView;
+    private TextView determinedTaxTextView;
+    private TextView doubleMinimumTaxTextView;
+    private TextView taxToBePayedTextView;
+    private TextView minimumMountToDeclareTextView;
+
+    private TableLayout tableLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rciva_dependientes);
+        getVisibleComponents();
+
+        totalIncomingEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if ( s.length() > 0 )
+                {
+                    String incomingText = s.toString();
+                    try
+                    {
+                        double totalIncoming = Double.parseDouble(incomingText);
+                        tableLayout.setVisibility(View.VISIBLE);
+                        updateForm(RCIVAcalc.calculate(totalIncoming));
+                    }
+                    catch (NumberFormatException nfe)
+                    {
+                        tableLayout.setVisibility(View.INVISIBLE);
+                        Toast.makeText(RCIVAActivity.this, "Ingreso total inválido", Toast.LENGTH_SHORT)
+                                .show();
+                    }
+                }
+                else
+                {
+                    tableLayout.setVisibility(View.INVISIBLE);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    private void getVisibleComponents()
+    {
+        this.tableLayout = (TableLayout)findViewById(R.id.tableLayout_rcivaForm);
+        this.tableLayout.setVisibility(View.INVISIBLE);
+
+        this.totalIncomingEditText = (EditText) findViewById(R.id.editText_totalIncoming);
+
+        this.bruteIncomingTextView = (TextView) findViewById(R.id.textView_bruteIncoming);
+        this.socialSecurityDiscountTextView = (TextView) findViewById(R.id.textView_socialSecurityDiscount);
+        this.doubleMinimumSalaryTextView = (TextView) findViewById(R.id.textView_twoMinimumSalary);
+        this.diffToDoubleMinimumSalaryTextView = (TextView) findViewById(R.id.textView_diffToDoubleMinimumSalary);
+        this.determinedTaxTextView = (TextView) findViewById(R.id.textView_determinedTax);
+        this.doubleMinimumTaxTextView = (TextView) findViewById(R.id.textView_doubleMinimumTax);
+        this.taxToBePayedTextView = (TextView) findViewById(R.id.textView_taxToBePayed);
+        this.minimumMountToDeclareTextView = (TextView) findViewById(R.id.textView_minimumMountToDeclare);
+    }
+
+    private void updateForm(RCIVAdata data)
+    {
+        this.setDoubleInTextView(this.bruteIncomingTextView, data.bruteIncoming);
+        this.setDoubleInTextView(this.socialSecurityDiscountTextView, data.socialSecurityDiscount);
+        this.setDoubleInTextView(this.doubleMinimumSalaryTextView, data.doubleMinimumSalary);
+        this.setDoubleInTextView(this.diffToDoubleMinimumSalaryTextView, data.diffToDoubleMinimumSalary);
+        this.setDoubleInTextView(this.determinedTaxTextView, data.determinedTax);
+        this.setDoubleInTextView(this.doubleMinimumTaxTextView, data.doubleMinimumTax);
+        this.setDoubleInTextView(this.taxToBePayedTextView, data.taxToBePayed);
+        this.setDoubleInTextView(this.minimumMountToDeclareTextView, data.minimumMountToDeclare);
+    }
+
+    private void setDoubleInTextView(TextView textView, double value) {
+        textView.setText( String.format("%.0f", value ) );
     }
 }
--- CalculadoraTributaria/app/src/main/java/com/alenasoft/caltrib/dependientes/RCIVAcalc.java
+++ CalculadoraTributaria/app/src/main/java/com/alenasoft/caltrib/dependientes/RCIVAcalc.java
@@ -5,21 +5,21 @@ package com.alenasoft.caltrib.dependientes;
  */
 public class RCIVAcalc {
 
-    private final double SOCIAL_SECURITY_FACTOR = 0.1271;
-    private final double DEFAULT_TAX = 0.13;
-    private final double MINIMUN_SALARY = 1805.00;
-    private final double INVOICES_PAY_FACTOR = 100.0/13.0;
+    public final static double SOCIAL_SECURITY_FACTOR = 0.1271;
+    public final static double DEFAULT_TAX = 0.13;
+    public final static double MINIMUM_SALARY = 1805.00;
+    public final static double INVOICES_PAY_FACTOR = 100.0/13.0;
 
-    public RCIVAdata calculate(double totalIncoming)
+    public static RCIVAdata calculate(double totalIncoming)
     {
         RCIVAdata data = new RCIVAdata();
 
         data.totalIncoming = totalIncoming;
-        data.socialSecurityDiscount = Math.round(data.totalIncoming * SOCIAL_SECURITY_FACTOR);
+        data.socialSecurityDiscount = Math.round(data.totalIncoming * RCIVAcalc.SOCIAL_SECURITY_FACTOR);
         data.bruteIncoming = data.totalIncoming - data.socialSecurityDiscount;
-        data.doubleMinimumSalary = MINIMUN_SALARY * 2;
+        data.doubleMinimumSalary = RCIVAcalc.MINIMUM_SALARY * 2;
         data.diffToDoubleMinimumSalary = data.bruteIncoming - data.doubleMinimumSalary;
-        data.determinedTax = Math.round(data.diffToDoubleMinimumSalary * DEFAULT_TAX);
+        data.determinedTax = Math.round(data.diffToDoubleMinimumSalary * RCIVAcalc.DEFAULT_TAX);
         data.doubleMinimumTax = Math.round(data.doubleMinimumSalary * DEFAULT_TAX);
         data.taxToBePayed = data.determinedTax - data.doubleMinimumTax;
         data.minimumMountToDeclare = Math.round(data.taxToBePayed * INVOICES_PAY_FACTOR);
