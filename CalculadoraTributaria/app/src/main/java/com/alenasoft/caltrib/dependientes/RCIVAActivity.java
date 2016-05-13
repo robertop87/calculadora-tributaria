c8744c146ce8936054fac1d2dac0c5f3945c2dda
Luis Roberto Perez Rios <luis.robertop87@gmail.com>
2016-05-12 22:01:21 -0400

    Added Copyright 2016 header

--- CalculadoraTributaria/app/src/main/java/com/alenasoft/caltrib/dependientes/RCIVAActivity.java
+++ CalculadoraTributaria/app/src/main/java/com/alenasoft/caltrib/dependientes/RCIVAActivity.java
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2016 Alenasoft
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software
+ * is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package com.alenasoft.caltrib.dependientes;
 
 import android.support.v7.app.AppCompatActivity;
@@ -12,6 +34,9 @@ import android.widget.Toast;
 
 import com.alenasoft.caltrib.R;
 
+/**
+ * Created by Luis Roberto Perez Rios <luis.robertop87@gmail.com> on 5/11/2016.
+ */
 public class RCIVAActivity extends AppCompatActivity {
 
     private EditText totalIncomingEditText;
